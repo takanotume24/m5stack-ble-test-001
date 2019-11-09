53686fbe0d974b5cb4e7d4cb765b73ddc8b37f9a
takuya matsunaga <takanotume24@gmail.com>
2019-11-09 19:32:53 +0900

    画面のチラツキを解決するためにマルチタスクにしたが動かなかった｡

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <M5Stack.h>
 #include "BLEDevice.h"
 
-uint8_t seq; // remember number of boots in RTC Memory
+uint8_t seq;                     // remember number of boots in RTC Memory
 #define MyManufacturerId 0xffff  // test manufacturer ID
-#define S_PERIOD     1  // Silent period
+#define S_PERIOD 1               // Silent period
 
 static BLEUUID service_uuid("");
 static BLEUUID char_uuid("");
@@ -14,25 +14,25 @@ static boolean do_connect = false;
 static boolean connected = false;
 static BLERemoteCharacteristic* p_remote_charastic;
 BLEScan* p_ble_scan;
-hw_timer_t * timer = NULL;
+hw_timer_t* timer = NULL;
 portMUX_TYPE timer_mux = portMUX_INITIALIZER_UNLOCKED;
 
-void IRAM_ATTR on_timer(){
-       // esp_deep_sleep(SLEEP_MSEC(1000));              // S_PERIOD秒Deep Sleepする
-    p_ble_scan->stop();
-
+void IRAM_ATTR on_timer() {
+  // esp_deep_sleep(SLEEP_MSEC(1000));              // S_PERIOD秒Deep Sleepする
+  p_ble_scan->stop();
 }
-void write_sd_card(float input){
-   File file = SD.open("/log.csv",FILE_APPEND);
-    file.printf("%f\n",input);
-    file.close();
+void write_sd_card(time_t input) {
+  File file = SD.open("/log.csv", FILE_APPEND);
+  file.printf("%f\n", input);
+  file.close();
 }
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertised_device) {
     if (!advertised_device.haveServiceUUID()) return;
-    if (!advertised_device.getServiceUUID().equals(BLEUUID(service_uuid))) return;
-    
+    if (!advertised_device.getServiceUUID().equals(BLEUUID(service_uuid)))
+      return;
+
     advertised_device.getScan()->stop();
     p_server_address = new BLEAddress(advertised_device.getAddress());
     do_connect = true;
@@ -40,55 +40,51 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
 };
 
 void setup() {
-    M5.begin();
+  M5.begin();
 
-    M5.Lcd.setTextSize(3);
+  M5.Lcd.setTextSize(3);
 
-    BLEDevice::init("");  
+  BLEDevice::init("");
 
-    p_ble_scan = BLEDevice::getScan();
-    p_ble_scan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-    p_ble_scan->setActiveScan(false);
+  p_ble_scan = BLEDevice::getScan();
+  p_ble_scan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  p_ble_scan->setActiveScan(false);
 
-    // timer = timerBegin(0,80,true);
-    // timerAttachInterrupt(timer,&on_timer,true);
-    // timerAlarmWrite(timer,100000, true);
-    // timerAlarmEnable(timer);
-    
+  // timer = timerBegin(0,80,true);
+  // timerAttachInterrupt(timer,&on_timer,true);
+  // timerAlarmWrite(timer,100000, true);
+  // timerAlarmEnable(timer);
 }
 
 void loop() {
-    float temp, humid, press, vbat;
+  float temp, humid, press, vbat;
+  time_t time;
+  struct tm* tm;
+  BLEScanResults foundDevices = p_ble_scan->start(1);  // スキャンする
+  int count = foundDevices.getCount();
+  for (int i = 0; i < count; i++) {
+    BLEAdvertisedDevice d = foundDevices.getDevice(i);
+    if (!d.haveManufacturerData()) continue;
 
-    BLEScanResults foundDevices = p_ble_scan->start(1);  // スキャンする
-    int count = foundDevices.getCount();
-    for (int i = 0; i < count; i++) {
-        BLEAdvertisedDevice d = foundDevices.getDevice(i);
-        if(!d.haveManufacturerData()){
-            continue;
-        }
-    
-        std::string data = d.getManufacturerData();
-        int manu = data[1] << 8 | data[0];
+    std::string data = d.getManufacturerData();
+    int manu = data[1] << 8 | data[0];
 
-        if (manu != MyManufacturerId || seq == data[2]) {  // カンパニーIDが0xFFFFで、
-            continue;
-        }             // シーケンス番号が新しいものを探す
+    if (manu != MyManufacturerId || seq == data[2])  // カンパニーIDが0xFFFFで、
+      continue;
+    // シーケンス番号が新しいものを探す
 
-        seq = data[2];
-        temp = (float)(data[4] << 8 | data[3]) / 100.0;  // パケットから温度を取得
-        humid = (float)(data[6] << 8 | data[5]) / 100.0;
-        press = (float)(data[8] << 8 | data[7]) * 10.0 / 100.0;
-        vbat = (float)(data[10] << 8 | data[9]) / 100.0;
+    seq = data[2];
+    time = (time_t)(data[6] << 24 | data[5] << 16 | data[4] << 8 | data[3]);  // パケットから温度を取得
+    tm = localtime(&time);
 
-        M5.Lcd.fillScreen(BLACK);
-        M5.Lcd.setCursor(20,  20); M5.Lcd.printf("seq: %d\r\n", seq);
-        M5.Lcd.setCursor(20,  60); M5.Lcd.printf("temp: %4.1f'C\r\n", temp);
-        M5.Lcd.setCursor(20, 100); M5.Lcd.printf("humid:%4.1f%%\r\n", humid);
-        M5.Lcd.setCursor(20, 140); M5.Lcd.printf("press:%4.0fhPa\r\n", press);
-        M5.Lcd.setCursor(20, 180); M5.Lcd.printf("vbat: %4.2fV\r\n", vbat);
-        write_sd_card(vbat);
-        delay(1000);
-    }
-    esp_deep_sleep(SLEEP_MSEC(1000)); 
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(20, 20);
+    M5.Lcd.printf("seq: %d\r\n", seq);
+    M5.Lcd.setCursor(20, 60);
+    M5.Lcd.printf("tm: %d:%d:%d'\r\n", tm->tm_hour,tm->tm_min,tm->tm_sec);
+
+    write_sd_card(time);
+    //delay(1000);
+  }
+  //esp_deep_sleep(SLEEP_MSEC(1000));
 }
